e5deae7511e8d44b51fa1566af66f9516202f910
elify <elifyagmurduran@gmail.com>
2023-11-26 17:35:03 +0100

    Update readingenvironment.sh

--- 3_readingtheenvironment/readingenvironment.sh
+++ 3_readingtheenvironment/readingenvironment.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# print the value of the HOME environment variable
+echo "HOME Environment Variable: $HOME"
+
+echo -e "\n"
+
+# print the value of the PATH environment variable
+echo "PATH Environment Variable: $PATH"
+
+echo -e "\n"
+
+# add a custom directory to the PATH variable
+custom_directory="/path/to/custom/directory"
+export PATH="$PATH:$custom_directory"
+
+# print the updated PATH environment variable
+echo "Updated PATH Environment Variable: $PATH"
