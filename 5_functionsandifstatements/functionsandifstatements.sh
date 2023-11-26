af22206346189d378ee5eff0b4bc163420ecde19
elify <elifyagmurduran@gmail.com>
2023-11-26 17:26:22 +0100

    Update functionsandifstatements.sh

--- 5_functionsandifstatements/functionsandifstatements.sh
+++ 5_functionsandifstatements/functionsandifstatements.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# function to check if a number is even or odd
+even_or_odd() {
+  # $1 represents the first argument passed to the function (the number)
+  number=$1
+  if [ $((number % 2)) -eq 0 ]; then
+    echo "$number is even."
+  else
+    echo "$number is odd."
+  fi
+}
+
+# call the function with different numbers
+even_or_odd 8
+even_or_odd 51
+even_or_odd 47
