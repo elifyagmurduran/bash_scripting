a8325b719c7aa9b456ff9cef6cee3f5f1e760865
elify <elifyagmurduran@gmail.com>
2023-11-26 15:32:08 +0100

    exercise 1

---
+++ helloworld/helloworld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is a simple Bash script
+
+# prompt the user for their name
+echo "enter your name:"
+read name
+
+# Greet the user
+echo "Hello, $name! Welcome to the world of Bash scripting."
