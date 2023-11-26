3a0b60a99e8e1a3b10c8a217e7f154a330da05f7
elify <elifyagmurduran@gmail.com>
2023-11-26 17:46:32 +0100

    Update readingenvironment.sh

--- 3_readingtheenvironment/readingenvironment.sh
+++ 3_readingtheenvironment/readingenvironment.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-# print the value of the HOME environment variable
-echo "HOME Environment Variable: $HOME"
-
-echo -e "\n"
-
-# print the value of the PATH environment variable
-echo "PATH Environment Variable: $PATH"
-
-echo -e "\n"
-
-# add a custom directory to the PATH variable
-custom_directory="C:\Users\elify\Documents\AndroidStudioProjects"
-export PATH="$PATH:$custom_directory"
-
-# print the updated PATH environment variable
-echo "Updated PATH Environment Variable: $PATH"
+s
