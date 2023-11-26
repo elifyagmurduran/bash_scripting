615759d6429fd0dac23b989f694a136f5b87b4f7
elify <elifyagmurduran@gmail.com>
2023-11-26 17:46:34 +0100

    Update readingexitcodes.sh

--- 4_readingexitcodes/readingexitcodes.sh
+++ 4_readingexitcodes/readingexitcodes.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# run a command that will succeed
+ls
+
+# read and print the exit code of the successful command
+exit_code=$?
+echo "exit code (success): $exit_code"
+
+# Run a command that will fail (e.g., ls /nonexistent)
+ls /nonexistent
+
+# Read and print the exit code of the failed command
+exit_code=$?
+echo "exit code (fail): $exit_code"
