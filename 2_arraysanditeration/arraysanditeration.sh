6947f322647e518e0b4c6e17952cd8a1b2881f12
elify <elifyagmurduran@gmail.com>
2023-11-26 16:35:12 +0100

    Update arraysanditeration.sh

--- 2_arraysanditeration/arraysanditeration.sh
+++ 2_arraysanditeration/arraysanditeration.sh
@@ -4,28 +4,30 @@
 arr1=("apple" "banana" "cherry" "tangerine" "dragonfruit")
 
 # iterate over the array
-echo "array elements:"
+echo -e "\n5 array elements:"
 for element in "${arr1[@]}"; do
   # print each element
-  echo "$element"
+  echo -e "$element"
 done
-
+echo -e "\nfirst round done\n"
+echo -e "add two more elements now:"
 # add two more elements to the array
-arr1+=("Fig" "Grape")
+arr1+=("orange" "grape")
 
 # iterate over the array
-echo "array elements:"
+
 for element in "${arr1[@]}"; do
   # print each element
-  echo "$element"
+  echo -e "$element"
 done
-
+echo -e "\nsecond round done\n"
+echo -e "delete the second element now:"
 # remove the second element from the array
 unset arr1[1]
 
 # iterate over the array
-echo "array elements:"
 for element in "${arr1[@]}"; do
   # print each element
-  echo "$element"
+  echo -e "$element"
 done
+echo -e "\nfin"
