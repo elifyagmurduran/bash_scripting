80c8f0bbabaccb9edcc5c74483db9f72c9f4e8cf
elify <elifyagmurduran@gmail.com>
2023-11-26 16:28:30 +0100

    file name org

--- 2_arraysanditeration/arraysanditeration.sh
+++ 2_arraysanditeration/arraysanditeration.sh
@@ -3,8 +3,22 @@
 # declare an array with at least 5 elements
 arr1=("apple" "banana" "cherry" "tangerine" "dragonfruit")
 
+# iterate over the array
+echo "array elements:"
+for element in "${arr1[@]}"; do
+  # print each element
+  echo "$element"
+done
+
 # add two more elements to the array
 arr1+=("Fig" "Grape")
+
+# iterate over the array
+echo "array elements:"
+for element in "${arr1[@]}"; do
+  # print each element
+  echo "$element"
+done
 
 # remove the second element from the array
 unset arr1[1]
