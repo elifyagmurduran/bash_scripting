ad8b54c7b33aed6e7def5bbae0c272fb7403033d
elify <elifyagmurduran@gmail.com>
2023-11-26 17:26:18 +0100

    Update arraysanditeration.sh

--- 2_arraysanditeration/arraysanditeration.sh
+++ 2_arraysanditeration/arraysanditeration.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
 # declare an array with at least 5 elements
-arr1=("apple" "banana" "cherry" "tangerine" "dragonfruit")
-
+arr=("apple" "banana" "cherry" "tangerine" "dragonfruit")
 # iterate over the array
 echo -e "\n5 array elements:"
-for element in "${arr1[@]}"; do
+for element in "${arr[@]}"; do
   # print each element
   echo -e "$element"
 done
 echo -e "\nfirst round done\n"
-echo -e "add two more elements now:"
+
 # add two more elements to the array
-arr1+=("orange" "grape")
+echo -e "add two more elements now:"
+arr+=("orange" "grape")
 
-# iterate over the array
-
-for element in "${arr1[@]}"; do
-  # print each element
+for element in "${arr[@]}"; do
   echo -e "$element"
 done
 echo -e "\nsecond round done\n"
-echo -e "delete the second element now:"
-# remove the second element from the array
-unset arr1[1]
 
-# iterate over the array
-for element in "${arr1[@]}"; do
-  # print each element
+# remove the second element from the array
+echo -e "delete the second element now:"
+unset arr[1]
+
+for element in "${arr[@]}"; do
   echo -e "$element"
 done
+
 echo -e "\nfin"
